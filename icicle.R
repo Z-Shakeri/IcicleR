1ea16d019fba2203d9211697524da04dc0ce342e
Z-Shakeri <lloi.shakeri@gmail.com>
2021-06-02 12:23:03 -0600

    Size of SB updated

--- icicle.R
+++ icicle.R
@@ -35,7 +35,7 @@ d3_tree <- sunburstR:::csv_to_hier(
 icicle_plot <- data_icicle %>%
   select('combined', 'per_patients')
 
-sb <- sunburst(d3_tree, withD3 = TRUE, colors = list(range = colors, domain = labels), width = 650)
+sb <- sunburst(d3_tree, withD3 = TRUE, colors = list(range = colors, domain = labels), width = 900)
 sb$x$tasks <- list(htmlwidgets::JS("
 function(){
   var chart = this.instance.chart;
